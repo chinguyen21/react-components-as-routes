7c8ac161d8d95fb9a0ddfedce9af2ba37a833513
Chi Nguyen <nguyen.thimai@northeastern.edu>
2021-02-10 17:28:48 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Navbar from './NavBar'
+import About from './About'
+import Home from './Home'
+import Login from './Login'
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
+
+  ReactDOM.render((
+    <Router>
+      <div>
+      <Navbar />
+      <Route exact path="/" component = {Home}/>
+      <Route exact path="/about" component = {About}/>
+      <Route exact path="/login" component = {Login}/>
+      </div>
+    </Router>
+  ),
+    document.getElementById('root')
   );
-};
-
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
-);
